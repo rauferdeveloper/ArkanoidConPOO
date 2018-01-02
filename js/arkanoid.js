f9b82f596824c08ca96129ed91346834c41ebb6b
rauferdeveloper <rauletecolegio@gmail.com>
2018-01-02 22:43:01 +0100

    creado 4 niveles en el juego

--- js/arkanoid.js
+++ js/arkanoid.js
@@ -12,11 +12,11 @@
         barra.crearBarra();
         contTopPastillaAumentarBarra=5;
         cantLadrillosDestruidos=0;
-        nivel=1;
+        nivel=4;
         ladrillos=new Array();
         ladrillosDobles=new Array();
         niveles  = new Niveles(nivel);
-        niveles.nivelUno();
+        niveles.nivelCuatro();
           
 }
 //Creamos la "clase" pelota
@@ -125,10 +125,15 @@ Pelota.prototype.moverBola=function(){
             }
             niveles.nivelDos();
           }        
-        this.colisionesLadrillo();
+        this.colisionessoloUnLadrillo();
 
     }else if(niveles.nivel==2){
         this.colisionesLadrilloDoble();
+
+    }else if(niveles.nivel==3){
+        this.colisionessoloUnLadrillo();
+    }else if(niveles.nivel==4){
+        this.colisionessoloUnLadrillo();
 
     }
 }
@@ -236,7 +241,7 @@ Pelota.prototype.colisionesBarra=function(){
     }
 }
 
-Pelota.prototype.colisionesLadrillo=function(){
+Pelota.prototype.colisionessoloUnLadrillo=function(){
     for ( i = 0; i < ladrillos.length; i++) {
         if(!ladrillos[i].destruido){
             if (this.top == ladrillos[i].top-this.height) {
@@ -478,7 +483,6 @@ Pelota.prototype.colisionesLadrilloDoble=function(){
        
 }    
     
-
 
 Pelota.prototype.colisionesPelotas=function(){
     for(i =0; i < pelotas.length;i++){
@@ -781,4 +785,147 @@ Niveles.prototype.nivelDos=function(){
             }   
     }
  
+}
+Niveles.prototype.nivelTres=function(){
+    if(this.nivel==3){
+            /* nivel 3*/
+            cantLadrillos=0;
+            ladrillosFila=12;
+            distancia=25;
+            aux=ladrillosFila;
+            fila=1;
+            maxLadrillos=96;
+            color="";
+            ladrillos=new Array(maxLadrillos);
+            posicionPastilla=Math.floor(Math.random()*ladrillos.length);// guardo la pastilla en una posicion al azar dentro del tamaño del array
+            alert("La pastilla se ha guardaddo en: "+posicionPastilla);
+            while(fila <=8){
+                if(fila==1){
+                    cantLadrillos=0;//si la fila es 1 su posicion en el for sera de 0-14
+                }else{
+                    cantLadrillos = ladrillosFila;//si antes por ejemplo era 0 pues sera ahora 14
+                    ladrillosFila += aux;//el maximo de ladrillos sera 14 pero aumentara para las posiciones del array
+                }
+                for (i = cantLadrillos; i< ladrillosFila; i++) {
+                         //cada fila tendra un color distinto
+                        if(fila==1){
+                            color="red";
+                        
+                
+                        }else  if(fila==2){
+                            color="aqua";
+                        
+                
+                        }else  if(fila==3){
+                            color="green";
+                        
+                
+                        }else  if(fila==4){
+                            color="orange";
+                        
+                
+                        }else  if(fila==5){
+                            color="white";
+                        
+                
+                        }else  if(fila==6){
+                            color="blue";
+                        
+                
+                        }else  if(fila==7){
+                            color="brown";
+                        
+                
+                        }else if(fila==8){
+                            color="teal";
+                        
+                
+                        }
+                        if(i==cantLadrillos){
+                            ladrillos[i]=new Ladrillo(i,50,20,distancia*fila,10,color);
+                        }else if(i==cantLadrillos+aux/2){
+                            ladrillos[i]=new Ladrillo(i,50,20,distancia*fila,ladrillos[i - 1].left + 50*3 + 5,color);
+
+                        }else{
+                            ladrillos[i]=new Ladrillo(i,50,20,distancia*fila,ladrillos[i - 1].left + 50 + 5,color);
+
+                        }
+                    }
+                   
+                
+                fila++;
+            }
+    }
+}
+Niveles.prototype.nivelCuatro=function(){
+    if(this.nivel==4){
+            /* nivel 4*/
+            cantLadrillos=0;
+            ladrillosFila=1;
+            distancia=25;
+            aux=2;
+            fila=1;
+            maxLadrillos=57;
+            color="";
+            ladrillos=new Array(maxLadrillos);
+            posicionPastilla=Math.floor(Math.random()*ladrillos.length);// guardo la pastilla en una posicion al azar dentro del tamaño del array
+            alert("La pastilla se ha guardaddo en: "+posicionPastilla);
+            while(fila <=8){
+                if(fila==1){
+                    cantLadrillos=0;//si la fila es 1 su posicion en el for sera de 0
+                }else{
+                    cantLadrillos = ladrillosFila;//si antes por ejemplo era 0 pues sera ahora 14
+                    ladrillosFila += aux;//el maximo de ladrillos sera 14 pero aumentara para las posiciones del array
+                    aux+=2;
+
+                }
+                for (i = cantLadrillos; i < ladrillosFila; i++) {
+                         //cada fila tendra un color distinto
+                        if(fila==1){
+                            color="red";
+                        
+                
+                        }else  if(fila==2){
+                            color="aqua";
+                        
+                
+                        }else  if(fila==3){
+                            color="green";
+                        
+                
+                        }else  if(fila==4){
+                            color="orange";
+                        
+                
+                        }else  if(fila==5){
+                            color="white";
+                        
+                
+                        }else  if(fila==6){
+                            color="blue";
+                        
+                
+                        }else  if(fila==7){
+                            color="brown";
+                        
+                
+                        }else if(fila==8){
+                            color="teal";
+                        
+                
+                        }
+                        if(i==cantLadrillos){
+                            ladrillos[i]=new Ladrillo(i,50,20,distancia*fila,10,color);
+                        }else{
+                            ladrillos[i]=new Ladrillo(i,50,20,distancia*fila,ladrillos[i - 1].left + 50 + 5,color);
+
+                        }
+                    }
+                   console.log("CantiLadrillos: "+cantLadrillos);
+                   console.log("ladrillosFila: "+ladrillosFila);
+                
+                fila++;
+
+            }
+    }
 }
